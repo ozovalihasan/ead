91f7ae10ccb89ace50013fa5b258ca1271f131f8
Hasan Ozovali <ozovalihasan@gmail.com>
2021-04-21 13:43:02 +0300

    Add last version of files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,163 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import initialData from './initial-data';
+import Column from './column';
 
-const  App = () => {
+const App = () => {
+  const [data, setData] = useState(initialData);
+
+  // const onDragStart = () => {
+  //   document.body.style.color = 'orange';
+  //   document.body.style.transition = 'background-color 0.2s ease';
+  // };
+
+  // const onDragUpdate = (update) => {
+  //   const { destination } = update;
+  //   const opacity = destination
+  //     ? destination.index / Object.keys(data.tasks).length
+  //     : 0;
+  //   document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity})`;
+  // };
+  const onDragStart = (start) => {
+    const homeIndex = data.columnOrder.indexOf(start.source.droppableId);
+
+    setData({
+      ...data,
+      homeIndex,
+    });
+  };
+  const onDragEnd = (result) => {
+    // document.body.style.color = 'inherit';
+    // document.body.style.backgroundColor = 'inherit';
+
+    const {
+      destination, source, draggableId, type,
+    } = result;
+
+    setData({
+      ...data,
+      homeIndex: null,
+    });
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId
+      && destination.index === source.index
+    ) {
+      return;
+    }
+
+    if (type === 'column') {
+      const newColumnOrder = Array.from(data.columnOrder);
+      newColumnOrder.splice(source.index, 1);
+      newColumnOrder.splice(destination.index, 0, draggableId);
+
+      const newData = {
+        ...data,
+        columnOrder: newColumnOrder,
+      };
+      setData(newData);
+      return;
+    }
+
+    const home = data.columns[source.droppableId];
+    const foreign = data.columns[destination.droppableId];
+
+    if (home === foreign) {
+      const newTaskIds = Array.from(home.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
+
+      const newColumn = {
+        ...home,
+        taskIds: newTaskIds,
+      };
+
+      const newData = {
+        ...data,
+        columns: {
+          ...data.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+
+      setData(newData);
+      return;
+    }
+
+    const startTaskIds = Array.from(home.taskIds);
+    startTaskIds.splice(source.index, 1);
+    const newStart = {
+      ...home,
+      taskIds: startTaskIds,
+    };
+
+    const finishTaskIds = Array.from(foreign.taskIds);
+    finishTaskIds.splice(destination.index, 0, draggableId);
+    const newFinish = {
+      ...foreign,
+      taskIds: finishTaskIds,
+    };
+
+    const newData = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newStart.id]: newStart,
+        [newFinish.id]: newFinish,
+      },
+    };
+    setData(newData);
+  };
+
   return (
     <div className="App">
-      
+      <DragDropContext
+        onDragStart={onDragStart}
+        // onDragUpdate={onDragUpdate}
+        onDragEnd={onDragEnd}
+      >
+
+        <Droppable
+          droppableId="all-columns"
+          direction="horizontal"
+          type="column"
+        >
+          {(provided) => {
+            <Container
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {data.columnOrder.map((columnId, index) => {
+                const column = data.columns[columnId];
+                const tasks = column.taskIds.map(
+                  (taskId) => data.tasks[taskId],
+                );
+                const isDropDisabled = index < data.homeIndex;
+                return (
+                  <Column
+                    key={column.id}
+                    column={column}
+                    tasks={tasks}
+                    isDropDisabled={isDropDisabled}
+                    index={index}
+                  />
+                );
+              })}
+              {provided.placeholder}
+            </Container>;
+          }}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
-}
+};
+
+const Container = styled.div`
+  display: flex;
+`;
 
 export default App;
---
+++ src/column.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
+import Task from './task';
+
+const Column = ({
+  column, tasks, isDropDisabled, index,
+}) => (
+  <Draggable draggableId={column.id} index={index}>
+    {(provided) => {
+      <Container
+        {...provided.draggableProps}
+        ref={provided.innerRef}
+      >
+        <Title {...provided.dragHandleProps}>
+          {column.title}
+        </Title>
+        <Droppable
+          droppableId={column.id}
+          type="task"
+          isDropDisabled={isDropDisabled}
+        >
+          {(provided, snapshot) => (
+            <TaskList
+              {...provided.droppableProps}
+              isDraggingOver={snapshot.isDraggingOver}
+              ref={provided.innerRef}
+            >
+              {tasks.map((task, index) => (
+                <Task key={task.id} task={task} index={index} />
+              ))}
+              { provided.placeholder }
+            </TaskList>
+          )}
+        </Droppable>
+      </Container>;
+    }}
+
+  </Draggable>
+);
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  background-color: white;
+  border-radius: 2px;
+  width: 220px;
+          
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.h3`
+  padding: 8px;
+`;
+
+const TaskList = styled.div`
+  padding: 8px;
+  transition: background-color 0.2s ease;
+  background-color: ${(props) => (props.isDraggingOver ? 'skyblue' : 'inherit')};
+  flex-grow: 1;
+  min-height: 100px;
+ 
+`;
+
+Column.propTypes = {
+  column: PropTypes.shape().isRequired,
+  tasks: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  isDropDisabled: PropTypes.bool.isRequired,
+  index: PropTypes.number.isRequired,
+};
+export default Column;

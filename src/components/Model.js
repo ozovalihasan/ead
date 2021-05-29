841ce24b69fdf63e4d3fe871849dbdf7328e23af
Hasan Ozovali <ozovalihasan@gmail.com>
2021-05-29 11:33:14 +0300

    Move AssociationButtons into HoverContainer

--- src/components/Model.js
+++ src/components/Model.js
@@ -121,6 +121,62 @@ const Model = ({
                     && (
                     <HoverContainer>
                       {
+                        id === restrictedDropId && (
+                          <RestrictedButtons>
+                              { (restrictedDropId !== -1)
+                            && allItems[restrictedDropId].entityClone
+                            && (id === restrictedDropId)
+                            && (
+                            <AssociationButtons>
+                              <AddAssociation onClick={() => {
+                                dispatch(addItem(2, id, 0, idCount));
+                                dispatch(updateRestrictedDropId(idCount, null));
+                                dispatch(idCountIncrease());
+                              }}
+                              >
+                                <svg height={20} viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+                                  <path
+                                    d="M 0,18 9,9 M 18,18 9,9 M 9,0 v 18"
+                                    stroke="black"
+                                    fill="transparent"
+                                  />
+                                </svg>
+                              </AddAssociation>
+                              <AddAssociation onClick={() => {
+                                dispatch(addItem(3, id, 0, idCount));
+                                dispatch(updateRestrictedDropId(idCount, null));
+                                dispatch(idCountIncrease());
+                              }}
+                              >
+                                <svg height={20} viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+                                  <path
+                                    d="M 9,0 9,18"
+                                    stroke="black"
+                                    fill="transparent"
+                                  />
+                                </svg>
+                              </AddAssociation>
+                              <AddAssociation onClick={() => {
+                                dispatch(addItem(4, id, 0, idCount));
+                                dispatch(updateRestrictedDropId(idCount, null));
+                                dispatch(idCountIncrease());
+                              }}
+                              >
+                                <svg height={20} viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+                                  <path
+                                    d="m 9,0 -0,3.25 m 0,3.5 -0,3.75 m 0,4.25 L 9,18"
+                                    stroke="black"
+                                    fill="transparent"
+                                  />
+                                </svg>
+                              </AddAssociation>
+
+                            </AssociationButtons>
+                            )}
+                          </RestrictedButtons>
+                        )
+                      }
+                      {
                         (allItems[id].factory || allItems[id].attribute)
                         // || compactMode
                         || (
@@ -247,56 +303,7 @@ const Model = ({
                       </CloneButton>
                     )
                   }
-                  { (restrictedDropId !== -1)
-                    && allItems[restrictedDropId].entityClone
-                    && (id === restrictedDropId)
-                    && (
-                    <AssociationButtons>
-                      <AddAssociation onClick={() => {
-                        dispatch(addItem(2, id, 0, idCount));
-                        dispatch(updateRestrictedDropId(idCount, null));
-                        dispatch(idCountIncrease());
-                      }}
-                      >
-                        <svg height={30} viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
-                          <path
-                            d="M 0,18 9,9 M 18,18 9,9 M 9,0 v 18"
-                            stroke="black"
-                            fill="transparent"
-                          />
-                        </svg>
-                      </AddAssociation>
-                      <AddAssociation onClick={() => {
-                        dispatch(addItem(3, id, 0, idCount));
-                        dispatch(updateRestrictedDropId(idCount, null));
-                        dispatch(idCountIncrease());
-                      }}
-                      >
-                        <svg height={30} viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
-                          <path
-                            d="M 9,0 9,18"
-                            stroke="black"
-                            fill="transparent"
-                          />
-                        </svg>
-                      </AddAssociation>
-                      <AddAssociation onClick={() => {
-                        dispatch(addItem(4, id, 0, idCount));
-                        dispatch(updateRestrictedDropId(idCount, null));
-                        dispatch(idCountIncrease());
-                      }}
-                      >
-                        <svg height={30} viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
-                          <path
-                            d="m 9,0 -0,3.25 m 0,3.5 -0,3.75 m 0,4.25 L 9,18"
-                            stroke="black"
-                            fill="transparent"
-                          />
-                        </svg>
-                      </AddAssociation>
 
-                    </AssociationButtons>
-                    )}
                   {
                     (
                       (

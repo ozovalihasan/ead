6a73b401eb20366b7d50bf78cb5083fe81ff109b
ozovalihasan <ozovalihasan@gmail.com>
2022-08-05 14:58:48 +0300

    Add an arrow to the ThroughEdge

--- src/ThroughEdge.tsx
+++ src/ThroughEdge.tsx
@@ -48,32 +48,59 @@ const ThroughEdge = ({
     targetY: ty,
   })
   
+  let orient: string = "0deg" 
+  if (targetPos === Position.Bottom) {
+    orient = "180deg"
+  } else if (targetPos === Position.Left) {
+    orient = "-90deg"
+  } else if (targetPos === Position.Right) {
+    orient = "90deg"
+  } else if (targetPos === Position.Top) {
+    orient = "0deg"
+  }
   
   return (
     <>
       
       <path
         id={`${id}-first`}
-        
         style={(selected || mouseOnEdge) ? {stroke: "black", strokeWidth: 3, strokeDasharray: 0} : style}
         className="stroke-first-500 fill-[none] stroke-[1] "
         d={throughEdgePath}
       />
 
       
+      <defs>
+        <marker   
+          orient={orient}
+          id={`through-arrow-${id}` }
+          viewBox="0 0 200 200"
+          refX="100" 
+          refY="0" 
+          markerUnits="strokeWidth" 
+          markerWidth="10px" 
+          markerHeight="10px"
+        >
+          <path 
+            d="M 200,-0.34918858 100,199.65081 0,-0.34918858 Z"
+            className="fill-first-500"  />
+        </marker>
+      </defs>
+      
       <path
         id={`${id}-second`}
         style={(selected || mouseOnEdge) ? {stroke: "black", strokeWidth: 3, strokeDasharray: 0} : style}
         className="stroke-first-500 fill-[none] stroke-[1] "
         d={edgePath}
+        markerEnd={`url(#through-arrow-${id})`}
       />
-      
       
       <path
         id={`${id}-second-wider`}
         className="fill-[none] stroke-[15] stroke-transparent"
         d={edgePath}
       />
+      
       { selected && 
         <foreignObject
           width={40}

6a191752631c3b52bdb5c61a9af1cedcd932e234
ozovalihasan <ozovalihasan@gmail.com>
2022-08-05 22:10:12 +0300

    Add a button to hide some elements

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -38,8 +38,7 @@ const Sidebar = () => {
   const showTextOnEdges = useStore(store => store.showTextOnEdges)
 
   return (
-    <aside className='relative border-x-2 border-solid py-3 px-2  w-1/5 overflow-y-scroll' >
-      <div className="mb-3 w-full">You can drag these nodes to the pane on the left.</div>
+    <aside className='relative border-x-2 border-solid py-3 px-2  w-1/5 overflow-y-scroll resize-x' >
       {
         
         Object.keys(tables).map((tableId: string) => {
@@ -81,49 +80,55 @@ const Sidebar = () => {
       <button className="p-1 bg-first-500 text-first-50 rounded-full aspect-square h-10" onClick={addTable}>+</button>
 
       
+      <details open className='mt-8 [&>summary>span:nth-child(1)]:open:hidden [&>summary>span:nth-child(2)]:open:inline '>
+        <summary>
+          <span>Show </span>
+          <span className='hidden'>Hide </span> 
+          menu
+        </summary>
+        <div className='mt-8 flex-col flex'>
+          <div className='flex mb-5'>
+            Show Association Names
+            <input className='ml-6' type="checkbox" onChange={toggleTextMode}/>
+          </div>
+          <div className='flex mb-5'>
+            Show the sidebar at the right of the window
+            <input className='ml-6' type="checkbox" checked={ locationSidebar === "right"} onChange={toggleLocationSidebar}/>
+          </div>
+          <button
+            className="rounded-md p-3 bg-first-500 border-2 border-first-500 my-1 ml-0 text-first-100"
+            onClick={() => saveJSON(useStore.getState(), 'EAD.json')}
+            type="button"
+            title="Download EAD"
+          >
+            Download EAD
+          </button>
+          
+          <label className='text-center p-3 rounded-md border-2 border-solid border-first-500 w-full cursor-pointer'>
+            Upload EAD
+            <input
+              className='hidden'
+              onChange={uploadStore}
+              type="file"
+              accept=".json"
+              data-testid="uploadInput"
+              title="Select an uploaded EAD"
+            />
+          </label>
+          
+          <button
+            className="mt-16 rounded-md p-3 border-2 border-first-500 my-1 mx-0 "
+            onClick={resetStore}
+            type="button"
+            title="Reset"
+          >
+            Reset
+          </button>
+          
 
-      <div className='p-2 mt-8 flex-col flex'>
-        <div className='flex mb-5'>
-          Show Association Names
-          <input className='ml-6' type="checkbox" onChange={toggleTextMode}/>
+              
         </div>
-        <div className='flex mb-5'>
-          Show the sidebar at the right of the window
-          <input className='ml-6' type="checkbox" checked={ locationSidebar === "right"} onChange={toggleLocationSidebar}/>
-        </div>
-        <button
-          className="rounded-md p-3 bg-first-500 border-2 border-first-500 my-1 ml-0 text-first-100"
-          onClick={() => saveJSON(useStore.getState(), 'EAD.json')}
-          type="button"
-          title="Download EAD"
-        >
-          Download EAD
-        </button>
-        
-        <label className='text-center p-3 rounded-md border-2 border-solid border-first-500 w-full cursor-pointer'>
-          Upload EAD
-          <input
-            className='hidden'
-            onChange={uploadStore}
-            type="file"
-            accept=".json"
-            data-testid="uploadInput"
-            title="Select an uploaded EAD"
-          />
-        </label>
-        
-        <button
-          className="mt-16 rounded-md p-3 border-2 border-first-500 my-1 mx-0 "
-          onClick={resetStore}
-          type="button"
-          title="Reset"
-        >
-          Reset
-        </button>
-        
-
-            
-      </div>
+      </details>
     </aside>
   );
 };

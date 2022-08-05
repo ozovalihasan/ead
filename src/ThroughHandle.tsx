58260ffc000f44ee556420ab595641c4d1a8c249
ozovalihasan <ozovalihasan@gmail.com>
2022-08-05 12:11:34 +0300

    Update the style of the associations handles

--- src/ThroughHandle.tsx
+++ src/ThroughHandle.tsx
@@ -1,5 +1,5 @@
 const ThroughHandle = ({visibility}: {visibility: string}) => (
-  <svg className={`w-6 h-6 absolute -bottom-3 left-3/4 bg-slate-700 rounded-full -translate-x-1/2 ${visibility}`} viewBox="0 0 200 200">
+  <svg className={`w-6 h-6 p-1 absolute -bottom-3 left-3/4 bg-slate-700 rounded-full -translate-x-1/2 ${visibility}`} viewBox="0 0 200 200">
     <path
       d="m 95,0 v 50 h 10 V 0"
       className="fill-slate-50"

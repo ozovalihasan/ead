dd9d8b469ae932d33d9dae39a6dab4023c31bd71
ozovalihasan <ozovalihasan@gmail.com>
2022-08-08 18:52:56 +0300

    Update the imported css file

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), externalCSSPlugin()]
+  plugins: [react()]
 })
 
-function externalCSSPlugin() {
-  return {
-    name: 'external-css',
-    transformIndexHtml: {
-      enforce: 'post',
-      transform(html, ctx) {
-        return [{
-          tag: "link",
-          attrs: {"rel": "stylesheet", "type":"text/css", "href": "./src/index.css"},
-          injectTo: "body-prepend",
-        }]
-      }
-    }
-  }
-}
+

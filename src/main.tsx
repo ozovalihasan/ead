0cec911b16c2573503aa665512fb388c0caa654a
ozovalihasan <ozovalihasan@gmail.com>
2022-08-08 18:18:29 +0300

    Update the imported styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css';
-import 'react-flow-renderer/dist/style.css';
-import 'react-flow-renderer/dist/theme-default.css';
 import './reset.css';
 
 

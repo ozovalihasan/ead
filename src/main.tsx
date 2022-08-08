31a0ad7011e223e1c07abd902e39d00adec024cb
ozovalihasan <ozovalihasan@gmail.com>
2022-08-08 17:16:41 +0300

    Remove the css files of the React Flow

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css';
 import './reset.css'
-import 'react-flow-renderer/dist/style.css';
-import 'react-flow-renderer/dist/theme-default.css';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

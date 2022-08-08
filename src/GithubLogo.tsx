7c2ef975e76e4c5b12a5643775088f35634570ff
ozovalihasan <ozovalihasan@gmail.com>
2022-08-08 14:30:34 +0300

    Update the logo of the link to the GitHub project

--- src/GithubLogo.tsx
+++ src/GithubLogo.tsx
@@ -1,6 +1,6 @@
 const GithubLogo = () => (
   <svg 
-    className="fill-first-500"
+    className="fill-white-500 bg-[white] rounded-full"
     xmlns="http://www.w3.org/2000/svg" 
     viewBox="0 0 496 512"
   >

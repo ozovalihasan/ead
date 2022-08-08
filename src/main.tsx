2f547d536e198c7158afd2dcfe716204825d4340
ozovalihasan <ozovalihasan@gmail.com>
2022-08-08 21:57:33 +0300

    Fix the wrong order of the stylesheets on production

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'react-flow-renderer/dist/style.css';
+import 'react-flow-renderer/dist/theme-default.css';
 import './reset.css';
 import './index.css';
-
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
-
-const links = document.getElementsByTagName("link")
-const link = links[links.length - 1]
-const head = document.getElementsByTagName("head")[0]
-
-head.append(link)

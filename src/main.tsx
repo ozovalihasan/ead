6a21d5bfb89d79dff0380ce1cde779c664f4e076
ozovalihasan <ozovalihasan@gmail.com>
2022-08-08 21:26:37 +0300

    Fix the wrong order of stylesheets on production

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   </React.StrictMode>
 )
 
-
 const links = document.getElementsByTagName("link")
 const link = links[links.length - 1]
 const head = document.getElementsByTagName("head")[0]

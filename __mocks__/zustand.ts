cac06dd55cf9a99bcb33e5a0546060343ae7c180
ozovalihasan <ozovalihasan@gmail.com>
2022-08-15 17:25:00 +0300

    Update the mock of zustand

--- __mocks__/zustand.ts
+++ __mocks__/zustand.ts
@@ -1,5 +1,4 @@
-import actualCreate from 'zustand'
-// const actualCreate = jest.requireActual('zustand') // if using jest
+const actualCreate = jest.requireActual('zustand') // if using jest
 import { act } from 'react-dom/test-utils'
 
 // a variable to hold reset functions for all stores declared in the app
@@ -7,7 +6,7 @@ const storeResetFns = new Set()
 
 // when creating a store, we get its initial state, create a reset function and add it in the set
 const create = (createState: any) => {
-  const store = actualCreate(createState)
+  const store = actualCreate.default(createState)
   const initialState = store.getState()
   storeResetFns.add(() => store.setState(initialState, true))
   return store

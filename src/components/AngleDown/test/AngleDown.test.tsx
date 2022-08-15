02e430b450c16a491a74966387f1159248fab3e8
ozovalihasan <ozovalihasan@gmail.com>
2022-08-15 11:24:10 +0300

    Update the test of the AngleDown

--- src/components/AngleDown/test/AngleDown.test.tsx
+++ src/components/AngleDown/test/AngleDown.test.tsx
@@ -1,19 +1,15 @@
-import {AngleDown} from './AngleDown';
-import { render, screen } from '@testing-library/react';
-import { Component } from 'react';
-import React from "react"
-
+import {AngleDown} from '../AngleDown';
+import { render } from '@testing-library/react';
 
 let renderReadyComponent: JSX.Element;
 
 beforeEach(() => {
-  
   renderReadyComponent = (
     <AngleDown></AngleDown>
   );
 });
 
-describe('<ListTrackedItems />', () => {
+describe('<AngleDown />', () => {
   it('renders correctly', () => {
     const renderedContainer = render(renderReadyComponent );
     expect(renderedContainer).toMatchSnapshot();

9ac77017733c7cb97d51e6d88cab387f79a655fe
ozovalihasan <ozovalihasan@gmail.com>
2022-08-17 12:19:16 +0300

    Add a test for the ConnectionLine component

---
+++ src/components/ConnectionLine/test/ConnectionLine.test.tsx
@@ -0,0 +1,71 @@
+import { ConnectionLine } from '../ConnectionLine';
+import { render, screen } from "@testing-library/react";
+import useStore from '@/zustandStore/store';
+import { Position } from 'react-flow-renderer';
+
+
+
+let renderReadyComponent: JSX.Element;
+
+beforeEach(() => {
+  
+  useStore.setState({ 
+    removeEdge: jest.fn() 
+  })
+  
+  renderReadyComponent = (
+    <ConnectionLine 
+      sourceX={10}
+      sourceY={20}
+      sourcePosition={Position.Bottom}
+      targetX={30}
+      targetY={40}
+      targetPosition={Position.Top}
+    />
+  );
+});
+
+describe('<ConnectionLine />', () => {
+  describe('if association type is "through"', () => {
+    
+    it('renders correctly', () => {
+      useStore.setState({ 
+        associationType: 'through',
+        nodes: [
+          {
+            "id": "1",
+            "type": "mockType",
+            "position": {
+                "x": 465,
+                "y": 150
+            },
+            "data": {
+                "tableId": "1",
+                "name": "mockName"
+            },
+            "width": 138,
+            "height": 45
+          },
+        ],
+        selectedNodeIdForThrough: "1"
+      })
+      
+      const renderedContainer = render(renderReadyComponent );
+      expect(renderedContainer).toMatchSnapshot();
+    });
+    
+  })
+
+  describe('if association type is"not "through"', () => {
+    it('renders correctly', () => {
+      useStore.setState({ 
+        associationType: 'has_many'
+      })
+      
+      const renderedContainer = render(renderReadyComponent );
+      expect(renderedContainer).toMatchSnapshot();
+    });
+    
+  })
+
+});

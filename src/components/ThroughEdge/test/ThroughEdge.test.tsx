755f0f00492fe6af196aa66341d68ba623f80066
ozovalihasan <ozovalihasan@gmail.com>
2022-08-18 12:10:52 +0300

    Add a test for the ThroughEdge component

---
+++ src/components/ThroughEdge/test/ThroughEdge.test.tsx
@@ -0,0 +1,174 @@
+import { ThroughEdge } from '../ThroughEdge';
+import { render, screen } from "@testing-library/react";
+import useStore from '@/zustandStore/store';
+import useCustomizationStore from '@/zustandStore/customizationStore';
+
+
+jest.mock('@/components',  () => ({
+  TargetHandle: (
+    ({ nodeId }: {nodeId: string} ) => (
+      <>
+        MockTargetHandle
+        nodeId: {nodeId}
+      </>
+    )
+  ),
+  TableName: (
+    ({ tableId }: {tableId: string} ) => (
+      <>
+        MockTableName
+        tableId: {tableId}
+      </>
+    )
+  ),
+  AllHandlers: (
+    ({ nodeId }: {nodeId: string} ) => (
+      <>
+        MockAllHandlers
+        nodeId: {nodeId}
+      </>
+    )
+  ),
+  SelectThroughNode: (
+    ({ nodeId }: {nodeId: string} ) => (
+      <>
+        MockSelectThroughNode
+        nodeId: {nodeId}
+      </>
+    )
+  ),
+      
+}))
+
+let renderReadyComponent: JSX.Element;
+
+beforeEach(() => {
+  
+  useStore.setState({ 
+    nodes: [
+      {
+        "id": "1",
+        "type": "entity",
+        "position": {
+            "x": 465,
+            "y": 150
+        },
+        "data": {
+            "tableId": "1",
+            "name": "Physician"
+        },
+        "width": 138,
+        "height": 45
+      },
+      {
+          "id": "2",
+          "type": "entity",
+          "position": {
+              "x": 585,
+              "y": 255
+          },
+          "data": {
+              "tableId": "2",
+              "name": "Appointment"
+          },
+          "width": 138,
+          "height": 45,
+          "selected": false,
+          "positionAbsolute": {
+              "x": 585,
+              "y": 255
+          },
+          "dragging": false
+      },
+      {
+          "id": "3",
+          "type": "entity",
+          "position": {
+              "x": 720,
+              "y": 150
+          },
+          "data": {
+              "tableId": "3",
+              "name": "Patient"
+          },
+          "width": 138,
+          "height": 45,
+          "selected": false,
+          "positionAbsolute": {
+              "x": 720,
+              "y": 150
+          },
+          "dragging": false
+      }
+    ]
+  })
+  
+  renderReadyComponent = (
+    <svg>
+      <ThroughEdge
+        id= {"111"}
+        source= {"1"}
+        target= {"3"}
+        label= {"through"}
+        data= {{throughNodeId: "2"}}
+        selected={false}
+      />
+    </svg>
+  );
+});
+
+describe('<ThroughEdge />', () => {
+  describe("if the mouse hovers on the edge", () => {
+    beforeEach(()=> {
+      useStore.setState({ 
+        mouseOnEdgeId: "111"
+      })
+    })
+
+    it('update styles ', () => {
+      
+      render(renderReadyComponent );
+
+      const allPathElements = screen.getAllByText("")[0].getElementsByTagName('path')
+
+      expect(allPathElements[0]).toHaveStyle({stroke: "black", strokeWidth: 3, strokeDasharray: 0});
+      expect(allPathElements[2]).toHaveStyle({stroke: "black", strokeWidth: 3, strokeDasharray: 0});
+      expect(allPathElements.length).toBe(4);
+    });
+  
+     it('renders correctly', () => {
+      useStore.setState({ 
+        associationType: 'has_many'
+      })
+      
+      const renderedContainer = render(renderReadyComponent );
+      expect(renderedContainer).toMatchSnapshot();
+    });
+    
+  })
+  
+  describe("if texts are shown on the edges", () => {
+    beforeEach(()=> {
+      useCustomizationStore.setState({ 
+        showTextOnEdges: true
+      })
+    })
+
+    it('shows the text ', () => {
+      
+      render(renderReadyComponent );
+      expect(screen.getByText(/through/i)).toBeInTheDocument();
+    });
+  
+    it('renders correctly', () => {
+      useStore.setState({ 
+        associationType: 'has_many'
+      })
+      
+      const renderedContainer = render(renderReadyComponent );
+      expect(renderedContainer).toMatchSnapshot();
+    });
+    
+  })
+
+});

ae3f752d3e1ab6d0af8026c21b1410003068da7b
ozovalihasan <ozovalihasan@gmail.com>
2022-08-18 21:17:45 +0300

    Update the style of the sidebar handle

--- src/App.tsx
+++ src/App.tsx
@@ -111,7 +111,7 @@ const App = () => {
           <div
               title="Click to hide/show the sidebar. Drag to change the width of the sidebar."
               onClick={toggleSidebarVisibility}
-              className="h-full border border-solid border-first-500 hover:cursor-move hover:bg-first-300 w-2 flex flex-col justify-center"
+              className="h-full border border-y-0 border-solid border-first-500 hover:cursor-move hover:bg-first-300 w-2 flex flex-col justify-center"
               draggable
               onDragEnd={handleSidebarWidthChange}
           >

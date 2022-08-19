fe1d4bccf61155c04cf185afbfd65b4b5677adeb
ozovalihasan <ozovalihasan@gmail.com>
2022-08-19 13:48:31 +0300

    Add a test for the Settings component

---
+++ src/components/Settings/test/Settings.test.tsx
@@ -0,0 +1,53 @@
+import { Settings } from '../Settings';
+import { render, screen, renderHook, fireEvent } from "@testing-library/react";
+import useCustomizationStore from '@/zustandStore/customizationStore';
+
+let renderReadyComponent: JSX.Element;
+
+beforeEach(() => {
+    
+useCustomizationStore.setState({ 
+    locationSidebar: "right",
+    showTextOnEdges: true,
+    toggleLocationSidebar: jest.fn(),
+    toggleTextMode: jest.fn(),
+  })
+    
+  renderReadyComponent = (
+    <Settings />
+  );
+});
+
+describe('<Settings />', () => {
+
+  it('renders a checkbox to change the location of sidebar', () => {
+
+    render(renderReadyComponent );
+    const { result } = renderHook(() => useCustomizationStore());
+
+    const labelElement = screen.getByText(/Show the sidebar at the right of the window/i)
+    const checkboxElement = labelElement.getElementsByTagName("input")[0]
+
+    expect(checkboxElement.checked).toBe(true)
+    
+    fireEvent.click(checkboxElement)
+
+    expect(result.current.toggleLocationSidebar).toHaveBeenCalledTimes(1);
+  });
+
+  it('renders a checkbox to change the visibility of edge names', () => {
+
+    render(renderReadyComponent );
+    const { result } = renderHook(() => useCustomizationStore());
+
+    const labelElement = screen.getByText(/Show Association Names/i)
+    const checkboxElement = labelElement.getElementsByTagName("input")[0]
+
+    expect(checkboxElement.checked).toBe(true)
+    
+    fireEvent.click(checkboxElement)
+
+    expect(result.current.toggleTextMode).toHaveBeenCalledTimes(1);
+  });
+
+});

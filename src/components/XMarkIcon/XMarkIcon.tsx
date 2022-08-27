15875083edba031a5581617389d1c21db0f8b6d1
ozovalihasan <ozovalihasan@gmail.com>
2022-08-27 15:41:20 +0300

    Update the XMarkIcon component

--- src/components/XMarkIcon/XMarkIcon.tsx
+++ src/components/XMarkIcon/XMarkIcon.tsx
@@ -4,7 +4,7 @@ export const XMarkIcon = () => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path 
-      className="stroke-[20px]"
+      className="stroke-[20px] animate-none!"
       d="m 20,180 h 0 L 180,20"
     />
 

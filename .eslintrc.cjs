43b439df756d5dd9785ac28d76be82bc9e870c9c
ozovalihasan <ozovalihasan@gmail.com>
2022-09-02 11:36:49 +0300

    Add eslint to the project

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/strict"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+};

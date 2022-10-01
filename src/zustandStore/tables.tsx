77a1b58f24caff879dc443f086b6f3367332f9fe
ozovalihasan <ozovalihasan@gmail.com>
2022-10-01 20:44:59 +0300

    Update indentations of the tables.jsx file

--- src/zustandStore/tables.tsx
+++ src/zustandStore/tables.tsx
@@ -13,34 +13,34 @@ export interface TableValueType {
 
 const initialTables: TablesType =  {
   "1": {
-      "name": "Physician",
-      "attributes": {
-          "13": {
-              "name": "name",
-              "type": "string"
-          }
-      },
-      "superclassId": ""
+    "name": "Physician",
+    "attributes": {
+      "13": {
+        "name": "name",
+        "type": "string"
+      }
+    },
+    "superclassId": ""
   },
   "2": {
-      "name": "Appointment",
-      "attributes": {
-          "12": {
-              "name": "appointment_date",
-              "type": "datetime"
-          }
-      },
-      "superclassId": ""
+    "name": "Appointment",
+    "attributes": {
+      "12": {
+        "name": "appointment_date",
+        "type": "datetime"
+      }
+    },
+    "superclassId": ""
   },
   "3": {
-      "name": "Patient",
-      "attributes": {
-          "14": {
-              "name": "name",
-              "type": "string"
-          }
-      },
-      "superclassId": ""
+    "name": "Patient",
+    "attributes": {
+      "14": {
+        "name": "name",
+        "type": "string"
+      }
+    },
+    "superclassId": ""
   }
 }
     

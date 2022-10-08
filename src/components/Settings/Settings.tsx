de046d665b5dec0f99d41d41acf871436b3cde0d
ozovalihasan <ozovalihasan@gmail.com>
2022-10-08 14:13:18 +0300

    Update the style of checkboxes

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -16,11 +16,11 @@ export const Settings = memo(() => {
       </summary>
       <div className='mt-8 flex-col flex'>
         <label className='flex mb-5 cursor-pointer'>
-          <input className='mr-6 accent-first-600' type="checkbox" checked={ showTextOnEdges} onChange={toggleTextMode}/>
+          <input className='mr-6' type="checkbox" checked={ showTextOnEdges} onChange={toggleTextMode}/>
           Show Association Names
         </label>
         <label className='flex mb-5 cursor-pointer'>
-          <input className='mr-6 ' type="checkbox" checked={ locationSidebar === "right"} onChange={toggleLocationSidebar}/>
+          <input className='mr-6' type="checkbox" checked={ locationSidebar === "right"} onChange={toggleLocationSidebar}/>
           Show the sidebar at the right of the window
         </label>
       </div>

45a619ffb3396a4035b8f0b34f7b3b695bb29032
ozovalihasan <ozovalihasan@gmail.com>
2022-10-10 17:20:02 +0300

    Remove an unnecessary describe function

--- src/zustandStore/helpers/test/transform_0_4_x_to_0_4_5.test.ts
+++ src/zustandStore/helpers/test/transform_0_4_x_to_0_4_5.test.ts
@@ -3,50 +3,48 @@ import { transform_0_4_x_to_0_4_5 } from '../transform_0_4_x_to_0_4_5';
 
 let data : unknown;
 
-describe('<ThroughEdge />', () => {
-  describe("transform_0_4_x_to_0_4_5 function", () => {
-   
-    it('adds superclassId attributes to tables if the version of data is not up-to-date ', () => {
-      data = {
-        version: "0.4.0",
-        tables: {
-          "1": {},
-          "2": {},
-        }
-      } 
-
-      expect(transform_0_4_x_to_0_4_5(data as State)).toStrictEqual({
-        version: "0.4.5",
-        tables: {
-          "1": {
-            "superclassId": "",
-          },
-          "2": {
-            "superclassId": "",
-          },
-        }
-      });
-      
-    });
-    
-    it('doesn"t make any change if the version of data is up-to-date ', () => {
-
-      data = {
-        version: "0.4.5",
-        tables: {
-          "1": {
-            superclassId: ""
-          },
-          "2": {
-            superclassId: ""
-          },
-        }
-      } 
-      
-      expect(transform_0_4_x_to_0_4_5(data as State)).toStrictEqual( data );
-      
-    });
-    
-  })
+describe("transform_0_4_x_to_0_4_5 function", () => {
   
-});
+  it('adds superclassId attributes to tables if the version of data is not up-to-date ', () => {
+    data = {
+      version: "0.4.0",
+      tables: {
+        "1": {},
+        "2": {},
+      }
+    } 
+
+    expect(transform_0_4_x_to_0_4_5(data as State)).toStrictEqual({
+      version: "0.4.5",
+      tables: {
+        "1": {
+          "superclassId": "",
+        },
+        "2": {
+          "superclassId": "",
+        },
+      }
+    });
+    
+  });
+  
+  it('doesn"t make any change if the version of data is up-to-date ', () => {
+
+    data = {
+      version: "0.4.5",
+      tables: {
+        "1": {
+          superclassId: ""
+        },
+        "2": {
+          superclassId: ""
+        },
+      }
+    } 
+    
+    expect(transform_0_4_x_to_0_4_5(data as State)).toStrictEqual( data );
+    
+  });
+  
+})
+  

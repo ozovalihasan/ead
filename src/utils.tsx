15f416033f3d4d54919157a44acbd919f5a2cff5
ozovalihasan <ozovalihasan@gmail.com>
2023-09-12 01:17:39 +0300

    Remove an unused function from utils

--- src/utils.tsx
+++ src/utils.tsx
@@ -104,35 +104,6 @@ export function getEdgeParams(source: Node, target: Node, isThrough = false, sou
   };
 }
 
-export function createNodesAndEdges() {
-  const nodes = [];
-  const edges = [];
-  const center = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-
-  nodes.push({ id: 'target', data: { label: 'Target' }, position: center });
-
-  for (let i = 0; i < 8; i++) {
-    const degrees = i * (360 / 8);
-    const radians = degrees * (Math.PI / 180);
-    const x = 250 * Math.cos(radians) + center.x;
-    const y = 250 * Math.sin(radians) + center.y;
-
-    nodes.push({ id: `${i}`, data: { label: 'Source' }, position: { x, y } });
-
-    edges.push({
-      id: `edge-${i}`,
-      target: 'target',
-      source: `${i}`,
-      type: 'floating',
-      markerEnd: {
-        type: MarkerType.Arrow,
-      },
-    });
-  }
-
-  return { nodes, edges };
-}
-
 export const positionToOrient = {
   [Position.Bottom]: "180deg",
   [Position.Left]: "-90deg",

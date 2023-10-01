3278334d4f4165881416bf345e291131771c8a8d
ozovalihasan <ozovalihasan@gmail.com>
2023-10-01 21:21:50 +0300

    Add a component to resize nodes

--- src/components/EntityNode/EntityNode.tsx
+++ src/components/EntityNode/EntityNode.tsx
@@ -4,15 +4,15 @@ import {
   SelectThroughNode,
   TargetHandle
 } from "@/components"
+import { ExpandToRight } from "@/icons";
 import { useStore, EntityNodeDataType } from '@/zustandStore';
 import { FC, memo, useRef } from "react";
-import { NodeProps } from "reactflow";
+import { NodeProps, NodeResizeControl, ResizeControlVariant } from "reactflow";
 
 export type EntityNodePropsType = FC<Pick<NodeProps<EntityNodeDataType>, "id" | "data" | "selected">> 
 
 export const EntityNode: EntityNodePropsType = memo(
-  ({id, data, selected }) => {
-    
+  ({id, data, selected}) => {
     const inputEl = useRef(null);
     
     const isSelectedNodeForThrough = useStore(store => store.selectedNodeIdForThrough === id)
@@ -25,25 +25,34 @@ export const EntityNode: EntityNodePropsType = memo(
     return (
       <div 
         className={`
-          p-1 rounded-md
+          p-1 rounded-md w-full h-full
           ${ nodeBGClasses}
-        `} 
+        `}
       >
-      
+        
         <TargetHandle nodeId={id} />
         
-        <input 
-          ref={inputEl}
-          placeholder='Entity' 
-          value={data.name} 
-          className="w-32 m-1 p-1 rounded-md ring-0 ring-offset-0" 
-          id="text" 
-          name="text" 
-          tabIndex={3}
-          onChange={event => onNodeInputChange(event, id)} 
-        />
-
+        <div className="w-auto p-1">
+          <input 
+            ref={inputEl}
+            placeholder='Entity' 
+            value={data.name} 
+            className="p-1 rounded-md ring-0 ring-offset-0 w-full" 
+            id="text" 
+            name="text" 
+            tabIndex={3}
+            onChange={event => onNodeInputChange(event, id)} 
+          />
+        </div>  
         <TableName nodeId={id} tableId={data.tableId} />
+        
+        {selected && 
+          <NodeResizeControl style={{border: "none"}} variant={ResizeControlVariant.Line} minWidth={114} >
+              <div className="fill-first-400 dark:fill-first-500 w-3 h-3 absolute -right-4 top-1/2 -translate-y-1/2 ">
+                <ExpandToRight/>
+              </div>
+          </NodeResizeControl>
+        }
         
         <AllHandlers nodeId={id} />
   
--- src/components/EntityNode/test/EntityNode.test.tsx
+++ src/components/EntityNode/test/EntityNode.test.tsx
@@ -1,6 +1,20 @@
 import { EntityNode } from '../EntityNode';
 import { fireEvent, render, renderHook, screen } from "@testing-library/react";
 import { useStore } from '@/zustandStore';
+import { ReactNode } from 'react';
+
+jest.mock('reactflow',  () => ({
+  NodeResizeControl: ({children}: {children: ReactNode}) => (
+    <>
+      MockNodeResizeControl
+      {children}
+    </>
+  ), 
+  ResizeControlVariant: {
+    Line: "line"
+  }
+  
+}))
 
 let renderReadyComponent: JSX.Element;
 
@@ -23,16 +37,20 @@ describe('<EntityNode />', () => {
   
   it('renders the necessary components correctly', () => {
     render(renderReadyComponent );
+
     expect(screen.getByText(/MockTargetHandle/i)).toBeInTheDocument();
     expect(screen.getByText(/MockTableName/i)).toBeInTheDocument();
     expect(screen.getByText(/MockAllHandlers/i)).toBeInTheDocument();
     expect(screen.getByText(/MockSelectThroughNode/i)).toBeInTheDocument();
+    expect(screen.getByText(/MockNodeResizeControl/i)).toBeInTheDocument();
+    expect(screen.getByText(/MockExpandToRight/i)).toBeInTheDocument();
   });
 
   it('renders an input with a placeholder "Entity"', () => {
+
     render(renderReadyComponent );
     const { result } = renderHook(() => useStore());
-    
+
     const inputElement = screen.getByPlaceholderText(/Entity/i)
 
     expect(inputElement).toBeInTheDocument();
@@ -43,28 +61,8 @@ describe('<EntityNode />', () => {
     expect(result.current.onNodeInputChange).toHaveBeenCalledTimes(1);
 
   });
-
-  it('renders an input with a placeholder "Entity"', () => {
-    render(renderReadyComponent );
-    const { result } = renderHook(() => useStore());
-  
-    const inputElement = screen.getByPlaceholderText(/Entity/i)
-
-    expect(inputElement).toBeInTheDocument();
-    expect(inputElement).toHaveValue("mockName");
-
-    fireEvent.change(inputElement, {target: {value: 'mockInput'}});
-    
-    expect(result.current.onNodeInputChange).toHaveBeenCalledTimes(1);
-
-  });
-
 
   it('renders correctly', () => {
-    useStore.setState({ 
-      associationType: 'has_many'
-    })
-    
     const renderedContainer = render(renderReadyComponent );
     expect(renderedContainer).toMatchSnapshot();
   });
